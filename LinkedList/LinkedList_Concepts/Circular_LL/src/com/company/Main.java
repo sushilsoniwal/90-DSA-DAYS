fb89bf76fd501eaf0392a7b3a4f57bd96058973c
Sushil Soniwal <sushilsoniwal1@gmail.com>
2022-04-17 17:54:58 +0530

    Day-24 Added Circular Linked List

---
+++ LinkedList/LinkedList_Concepts/Circular_LL/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CLL list = new CLL();
+
+        /* ********** INSERTION IN CIRCULAR LINKED LIST ********** */
+        System.out.print("List after inserting at first position:- ");
+        list.insertFirst(50);
+        list.insertFirst(30);
+        list.insertFirst(22);
+        list.insertFirst(14);
+        list.insertFirst(13);
+        list.display();
+        System.out.println("The size is:- " + list.size);
+
+        System.out.print("List after inserting at last position:- ");
+        list.insertLast(3);
+        list.insertLast(5);
+        list.display();
+        System.out.println("The size is:- " + list.size);
+
+        System.out.print("List after inserting at given position:- ");
+        list.insert(92, 0);
+        list.insert(82, 8);
+        list.insert(72, 4);
+        list.display();
+        System.out.println("The size is:- " + list.size);
+
+        /* ********** DELETION IN CIRCULAR LINKED LIST ********** */
+        System.out.print("list after deleting the first node:- ");
+        list.deleteFirst();
+        list.display();
+        System.out.println("The size is:- " + list.size);
+
+        System.out.print("list after deleting the last node:- ");
+        list.deleteLast();
+        list.display();
+        System.out.println("The size is:- " + list.size);
+
+        System.out.print("list after deleting the desired node:- ");
+        list.delete(3);
+        list.display();
+        System.out.println("The size is:- " + list.size);
+    }
+}
